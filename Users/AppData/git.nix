ceae659f9d9790ea77f10fe4fe5739a1b7407a04
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2024-10-27 10:29:49 +0800

    Enable sign

--- Users/AppData/git.nix
+++ Users/AppData/git.nix
@@ -1,8 +1,27 @@
 {config, ...}: {
   programs.git = {
     enable = true;
-    userName = "MiRinChan";
-    userEmail = "148533509+MiRinChan@users.noreply.github.com";
+    config = {
+      init = {
+        defaultBranch = "master";
+      };
+      url = {
+        "https://github.com/" = {
+          insteadOf = [
+            "gh:"
+            "github:"
+          ];
+        };
+      };
+      user = {
+        name = "MiRinChan";
+        email = "148533509+MiRinChan@users.noreply.github.com";
+        signingKey = "08A5B955EF921DC3";
+      };
+      commit = {
+        gpgsign = true;
+      };
+    };
   };
   programs.gh = {
     enable = true;

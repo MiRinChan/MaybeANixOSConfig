6596ac50ae09e9a40c5ae99e2552940257a54ba9
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2024-12-14 20:42:17 +0800

    moew

--- modules/nixos/ProgramFiles/common.nix
+++ modules/nixos/ProgramFiles/common.nix
@@ -26,14 +26,6 @@
     pkgs.sbctl # For debugging and troubleshooting Secure Boot.
     libarchive # provide bsdcat bsdcpio bsdtar bsdunzip
     jq # provide a json proceessor
-    kdePackages.kaccounts-integration # provide online account login
-    kdePackages.kaccounts-providers # provide online account login
-    kdePackages.signond # provide online account login
-    kdePackages.kio-gdrive # provide Google Drive
-    kdePackages.kio-extras
-    kdePackages.plasma-browser-integration
-    kdePackages.kio
-    signon-plugin-oauth2
   ];
   programs.zsh.enable = true;
   programs.gnupg.agent = {

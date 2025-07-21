69df0dba838bf8dbab00ea129332a030c8f64958
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2025-07-21 10:40:58 +0800

    modified:   Users/AppData/AboutProgramming.nix

--- Users/AppData/AboutProgramming.nix
+++ Users/AppData/AboutProgramming.nix
@@ -8,7 +8,7 @@
     # package = pkgs.vscode.override {commandLineArgs = "--locale=zh-CN --enable-features=UseOzonePlatform,WaylandWindowDecorations --ozone-platform=wayland --enable-wayland-ime ";};
     package = pkgs.vscode.override {commandLineArgs = "--enable-wayland-ime -r";};
   };
-
+  programs.direnv.enable = true;
   home.packages = with pkgs; [
     # 编程环境
     devbox

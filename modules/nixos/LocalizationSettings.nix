8bab64f2dcee4cfd2169ec1ea6c653839cf1bee8
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2025-08-04 17:21:13 +0800

    Add addon

--- modules/nixos/LocalizationSettings.nix
+++ modules/nixos/LocalizationSettings.nix
@@ -52,6 +52,9 @@
     fcitx5.waylandFrontend = true;
     fcitx5.addons = with pkgs; [
       fcitx5-chinese-addons
+      fcitx5-pinyin-moegirl
+      fcitx5-pinyin-zhwiki
+      fcitx5-pinyin-minecraft
     ];
   };
   # i18n.inputMethod = {

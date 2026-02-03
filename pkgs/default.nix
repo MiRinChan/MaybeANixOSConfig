388762cc34b1c99c5f9db03930ae028cb8af604c
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2026-02-03 23:48:17 +0800

    移除klassy依赖并添加klassy-qt6包

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,4 +7,5 @@ pkgs: {
   rquickshare-the-legacy = pkgs.callPackage ./RQuickShare.nix {};
   scrcpy3 = pkgs.callPackage ./scrcpy3.nix {};
   # wallpaper-engine-kde-plugin = pkgs.callPackage ./wallpaper-engine-kde-plugin {};
+  klassy-qt6 = pkgs.callPackage ./klassy-qt6 {};
 }

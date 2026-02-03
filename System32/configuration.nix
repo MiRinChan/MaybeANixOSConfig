e7e82f0306cdd59eabb6b8b3b14468ecc381791c
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2026-02-03 23:34:48 +0800

    modified:   System32/configuration.nix

--- System32/configuration.nix
+++ System32/configuration.nix
@@ -13,7 +13,6 @@
     outputs.nixosModules.theProgramInstallForAllUsers
     outputs.nixosModules.localizationSettings
     outputs.nixosModules.nvidiaDriver
-    outputs.nixosModules.ADBDriver
     outputs.nixosModules.soundSettings
     outputs.nixosModules.FHSEnviroment
 

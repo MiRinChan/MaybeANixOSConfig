062aa26e2181dc788425a7dbb56649614e2a63a2
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2026-03-05 15:56:15 +0800

    Upgrade

--- flake.nix
+++ flake.nix
@@ -3,8 +3,7 @@
 
   inputs = {
     ### Nixpkgs ###
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # Pin 特定频道
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";

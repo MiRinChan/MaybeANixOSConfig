6d49e178c49a04ea173daa9582f78f6c54c18f67
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2026-04-20 17:43:15 +0800

    1

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,4 +8,5 @@ pkgs: {
   scrcpy3 = pkgs.callPackage ./scrcpy3.nix {};
   # wallpaper-engine-kde-plugin = pkgs.callPackage ./wallpaper-engine-kde-plugin {};
   klassy-qt6 = pkgs.callPackage ./klassy-qt6 {};
+  iloader = pkgs.callPackage ./iloader.nix {};
 }

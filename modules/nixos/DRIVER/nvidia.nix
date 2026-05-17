30a760f76c5ae29a7cbfc26e1e637b3bf5578296
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2026-05-17 20:17:38 +0800

    fix: switch NVIDIA driver package from stable to beta

--- modules/nixos/DRIVER/nvidia.nix
+++ modules/nixos/DRIVER/nvidia.nix
@@ -30,8 +30,8 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    # package = config.boot.kernelPackages.nvidiaPackages.beta;
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
     # package = config.boot.kernelPackages.nvidiaPackages.production;
   };
   # Load nvidia driver for Xorg and Wayland

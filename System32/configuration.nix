5cbaf318327a3e9cea3f6a29ddbf52510603d837
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2026-06-14 18:11:13 +0800

    refactor: remove permitted insecure package for electron

--- System32/configuration.nix
+++ System32/configuration.nix
@@ -254,10 +254,6 @@
     SUBSYSTEM=="usb", ATTR{idVendor}=="04e8", MODE="0666", GROUP="plugdev"
   '';
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-39.8.10"
-  ];
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
 }
